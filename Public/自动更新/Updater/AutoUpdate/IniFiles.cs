be9b3392accf1298723ff7bf79d0360483f360d0
xuanximoming <yeshentianyue@sina.com>
2018-05-17 23:28:27 +0800

    jj

--- Public/自动更新/Updater/AutoUpdate/IniFiles.cs
+++ Public/自动更新/Updater/AutoUpdate/IniFiles.cs
@@ -1,34 +1,33 @@
-using System;
 using System.Runtime.InteropServices;
 using System.Text;
 
 namespace AutoUpdate
 {
-	public class IniFiles
-	{
-		public string path;
+    public class IniFiles
+    {
+        public string path;
 
-		[DllImport("kernel32")]
-		private static extern long WritePrivateProfileString(string section, string key, string val, string filePath);
+        [DllImport("kernel32")]
+        private static extern long WritePrivateProfileString(string section, string key, string val, string filePath);
 
-		[DllImport("kernel32")]
-		private static extern int GetPrivateProfileString(string section, string key, string def, StringBuilder retVal, int size, string filePath);
+        [DllImport("kernel32")]
+        private static extern int GetPrivateProfileString(string section, string key, string def, StringBuilder retVal, int size, string filePath);
 
-		public IniFiles(string INIPath)
-		{
-			this.path = INIPath;
-		}
+        public IniFiles(string INIPath)
+        {
+            this.path = INIPath;
+        }
 
-		public void IniWritevalue(string Section, string Key, string value)
-		{
-			IniFiles.WritePrivateProfileString(Section, Key, value, this.path);
-		}
+        public void IniWritevalue(string Section, string Key, string value)
+        {
+            IniFiles.WritePrivateProfileString(Section, Key, value, this.path);
+        }
 
-		public string IniReadvalue(string Section, string Key)
-		{
-			StringBuilder stringBuilder = new StringBuilder(255);
-			int privateProfileString = IniFiles.GetPrivateProfileString(Section, Key, "", stringBuilder, 255, this.path);
-			return stringBuilder.ToString();
-		}
-	}
+        public string IniReadvalue(string Section, string Key)
+        {
+            StringBuilder stringBuilder = new StringBuilder(255);
+            int privateProfileString = IniFiles.GetPrivateProfileString(Section, Key, "", stringBuilder, 255, this.path);
+            return stringBuilder.ToString();
+        }
+    }
 }
