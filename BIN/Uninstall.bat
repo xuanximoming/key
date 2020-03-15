ab118c294c7d14e41ea67e967d7a17147e1d19b9
xuanximoming <yeshentianyue@sina.com>
2020-03-15 12:24:55 +0800

    20200315

---
+++ BIN/Uninstall.bat
@@ -0,0 +1,2 @@
+net stop "JobService"
+%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\InstallUtil.exe JobManagerService.exe /u
---
+++ BIN/installsvc.bat
@@ -0,0 +1,2 @@
+%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\InstallUtil.exe JobManagerService.exe
+pause
